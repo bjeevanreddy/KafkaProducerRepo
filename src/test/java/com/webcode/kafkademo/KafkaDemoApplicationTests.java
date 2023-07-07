79c71da3e5dce883e7ad948a4f4acec82adb9c11
bjeevanreddy45 <bjeevanreddy45@gmail.com>
2023-07-07 02:07:51 -0500

    kafak Producer and kafka setup,

---
+++ src/test/java/com/webcode/kafkademo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcode.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
